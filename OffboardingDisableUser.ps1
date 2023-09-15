e5914feb6326314884925898f276a2d5718a6f0a
Chris de Bruijn <xevichris@gmail.com>
2023-09-15 11:56:57 +0200

    upn instead of logonname

--- OffboardingDisableUser.ps1
+++ OffboardingDisableUser.ps1
@@ -1,30 +1,34 @@
 # Import the Active Directory module
 Import-Module ActiveDirectory
 
-# Define the path to the CSV file containing user logon names of employees leaving
+# Define the path to the CSV file containing user information
 $csvPath = "S:\Fileshare\HR\LeavingHires.csv"
 
-# Define the target OU where you want to move the former employees
+# Define the target OU where you want to move former employees
 $ouPath = "OU=Former Employees,DC=CDB,DC=lan"
 
 # Check if the CSV file exists
 if (Test-Path $csvPath) {
     # Read the CSV file
-    $leavingUserList = Import-Csv $csvPath
+    $userList = Import-Csv $csvPath
 
     # Loop through each user in the CSV
-    foreach ($user in $leavingUserList) {
-        $logonName = $user.LogonName
+    foreach ($user in $userList) {
+        $upn = $user.UPN  # UPN from the CSV
 
         # Check if the user exists
-        $existingUser = Get-ADUser -Filter { (SamAccountName -eq $logonName) } -ErrorAction SilentlyContinue
+        $existingUser = Get-ADUser -Filter { (UserPrincipalName -eq $upn) } -ErrorAction SilentlyContinue
 
         if ($existingUser -ne $null) {
-            # Disable the user account
-            Disable-ADAccount -Identity $logonName
+            try {
+                # Disable the user account
+                Disable-ADAccount -Identity $upn
 
-            # Move the user to the "OU=Former Employees" OU
-            Move-ADObject -Identity $existingUser -TargetPath $ouPath
-        }
+                # Move the user to the former employees OU
+                Move-ADObject -Identity $upn -TargetPath $ouPath -ErrorAction Stop
+            } catch {
+                Write-Host "Error offboarding user '$upn': $_"
+            }
+        } 
     }
 }
